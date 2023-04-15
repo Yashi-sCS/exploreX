d5b9580e8ed43ddaf3685e1c5b184b356fd2633e
Yashi-sCs <yashi.saxena2021@gmail.com>
2023-04-15 17:08:41 +0530

    place

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles';
 
 const PlaceDetails = ({place}) => {
+
+    const classes =useStyles();
+
     return(
-        <h1>{place.name}</h1>
+        <Card elevation={6}>
+
+        </Card>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
+// import React from 'react';
+//  import ReactDOM from 'react-dom';
+
+// import App from './App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
 import React from 'react';
-import  ReactDOM  from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container);
+root.render(<App />);
+
+
+
