d1cf22e743208e3736daf73cac27c8927aa5721c
Yashi-sCs <yashi.saxena2021@gmail.com>
2023-04-16 00:12:16 +0530

    display places

--- src/App.js
+++ src/App.js
@@ -60,6 +60,7 @@ const App=()=>{
                         setCoordinates={setCoordinates}
                         setBounds={setBounds}
                         coordinates={coordinates}
+                        places={places}
                     />
                 </Grid>
 
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -23,7 +23,40 @@ title={place.name}
     <Typography variant="subtitle1">Price </Typography>
     <Typography gutterBottom variant="subtitle1">{place.price_level} </Typography>
 </Box>
+<Box display="flex" justifyContent="space-between" >
+    <Typography variant="subtitle1">Ranking </Typography>
+    <Typography gutterBottom variant="subtitle1">{place.ranking} </Typography>
+</Box>
+{place?.awards?.map((award) => (
+          <Box display="flex" justifyContent="space-between" my={1} alignItems="center">
+            <img src={award.images.small} />
+            <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
+          </Box>
+        ))}
+        {place?.cuisine?.map(({ name }) => (
+          <Chip key={name} size="small" label={name} className={classes.chip} />
+        ))}
+        {place.address && (
+          <Typography gutterBottom variant="body2" color="textSecondary" className={classes.subtitle}>
+            <LocationOnIcon />{place.address}
+          </Typography>
+        )}
+        {place.phone && (
+          <Typography variant="body2" color="textSecondary" className={classes.spacing}>
+            <PhoneIcon /> {place.phone}
+          </Typography>
+        )}
+
+
 </CardContent>
+<CardActions>
+        <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')}>
+        Trip Advisor
+        </Button>
+        <Button size="small" color="primary" onClick={() => window.open(place.website, '_blank')}>
+          Website
+        </Button>
+      </CardActions>
 
         </Card>
     );
