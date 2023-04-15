e90de431239693663bfe6567c5205d16bc7a3fff
Yashi-sCs <yashi.saxena2021@gmail.com>
2023-04-15 18:13:37 +0530

    PlaceCard

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -19,6 +19,10 @@ title={place.name}
 />
 <CardContent>
 <Typography gutterBottom variant="h5">{place.name}</Typography>
+<Box display="flex" justifyContent="space-between" >
+    <Typography variant="subtitle1">Price </Typography>
+    <Typography gutterBottom variant="subtitle1">{place.price_level} </Typography>
+</Box>
 </CardContent>
 
         </Card>
