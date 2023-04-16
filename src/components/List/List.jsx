9d781ea3793056e93a1430e09d03706014ca00a0
Yashi-sCs <yashi.saxena2021@gmail.com>
2023-04-16 20:29:25 +0530

    done

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,36 +1,39 @@
-import React, { useState, useEffect, createRef } from 'react';
-import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
-
-
+import React, {useState,useEffect,createRef} from "react";
+import { CircularProgress, Grid, Typography, MenuItem,FormControl,Select, InputLabel } from "@material-ui/core";
 import PlaceDetails from '../PlaceDetails/PlaceDetails';
-import useStyles from './styles.js';
 
-const List = ({ places, type, setType, rating, setRating, childClicked, isLoading }) => {
-  const [elRefs, setElRefs] = useState([]);
-  const classes = useStyles();
+import useStyles from './styles';
 
-  useEffect(() => {
-    setElRefs((refs) => Array(places.length).fill().map((_, i) => refs[i] || createRef()));
-  }, [places]);
+const List = ({places , childClicked , isLoading ,type, setType,rating,setRating}) => {
+    
+const[elRefs, setElRefs] =useState([]);
+const classes = useStyles();
 
-  return (
-    <div className={classes.container}>
-      <Typography variant="h4">Food & Dining around you</Typography>
-      {isLoading ? (
-        <div className={classes.loading}>
-          <CircularProgress size="5rem" />
-        </div>
-      ) : (
-        <>
-          <FormControl className={classes.formControl}>
-            <InputLabel id="type">Type</InputLabel>
-            <Select id="type" value={type} onChange={(e) => setType(e.target.value)}>
-              <MenuItem value="restaurants">Restaurants</MenuItem>
-              <MenuItem value="hotels">Hotels</MenuItem>
-              <MenuItem value="attractions">Attractions</MenuItem>
+useEffect(() => {
+ const refs =Array(places?.length).fill().map((_,i)=> elRefs[i] || createRef());
+
+ setElRefs(refs);
+},[places]);
+
+    return(
+       <div className={classes.container}>
+        <Typography variant="h4">Restaurants, Hotels & Attractions around you</Typography>
+        {isLoading ? (
+          <div className={classes.loading}>
+            <CircularProgress size="5rem"/>
+            </div>
+        ) : (
+          <>
+        <FormControl className={classes.formControl}>
+            <InputLabel>Type</InputLabel>
+            <Select value={type} onChange={(e) => setType(e.target.value)}>
+            <MenuItem value="restaurants">Restaurants</MenuItem>
+            <MenuItem value="hotels">Hotels</MenuItem>
+            <MenuItem value="attractions">Attractions</MenuItem>
             </Select>
-          </FormControl>
-          <FormControl className={classes.formControl}>
+        </FormControl>
+
+        <FormControl className={classes.formControl}>
             <InputLabel id="rating">Rating</InputLabel>
             <Select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
               <MenuItem value="">All</MenuItem>
@@ -39,17 +42,18 @@ const List = ({ places, type, setType, rating, setRating, childClicked, isLoadin
               <MenuItem value="4.5">Above 4.5</MenuItem>
             </Select>
           </FormControl>
+
           <Grid container spacing={3} className={classes.list}>
             {places?.map((place, i) => (
-              <Grid ref={elRefs[i]} key={i} item xs={12}>
-                <PlaceDetails selected={Number(childClicked) === i} refProp={elRefs[i]} place={place} />
+               <Grid key={i} item xs={12}>
+               <PlaceDetails selected={Number(childClicked) === i} refProp={elRefs[i]} place={place} />
               </Grid>
             ))}
           </Grid>
-        </>
-      )}
-    </div>
-  );
-};
+          </>
+        )}
+       </div>
+    );
+}
 
 export default List;
