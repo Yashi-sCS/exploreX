8d4e7a812445857985faab1080cf9e760f50e5b9
Yashi-sCs <yashi.saxena2021@gmail.com>
2023-04-16 19:23:38 +0530

    Weather

--- src/api/index.js
+++ src/api/index.js
@@ -31,3 +31,21 @@ const URL ='https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
         return null;
     }
 }
+
+export const getWeatherData = async (lat, lng) => {
+    try {
+      if (lat && lng) {
+        const { data } = await axios.get('https://weather1395.p.rapidapi.com/temperature', {
+            params: {url: 'Casablanca'},
+            headers: {
+              'X-RapidAPI-Key': 'ae2d36b64cmshfbea10d7436e0eep1bc842jsnb7a7060ef86b',
+              'X-RapidAPI-Host': 'weather1395.p.rapidapi.com'
+            }
+        });
+  
+        return data;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
